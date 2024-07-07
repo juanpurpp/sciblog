9ee1fdca11034fe43966757f27cf998bf7f44e24
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2024-07-07 16:23:39 -0400

    arreglo boton eliminar foro

--- src/components/ForoPost.js
+++ src/components/ForoPost.js
@@ -1,30 +1,34 @@
 "use client";
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { obtenerTemas } from '@/queries/foro';
-import { useSession } from 'next-auth/react'; // Importa useSession de Next.js
+import { useSession } from 'next-auth/react';
 import FotoForo from './FotoForo'
 import Link from 'next/link';
-const ForoPost = ({ className }) => {
-  const { data, isLoading } = useQuery(['get-foros'], () => obtenerTemas());
-  const { data: session } = useSession(); // Obtener los datos de quien lo esta usando ahora
 
-  console.log('data', data)
+const ForoPost = ({ className }) => {
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery(['get-foros'], obtenerTemas);
+  const { data: session } = useSession();
+
+  const handleDeleteSuccess = () => {
+    queryClient.invalidateQueries(['get-foros']);
+  };
+
   return (
     <div className={className}>
       <div className="flex flex-col w-full">
         <div className="relative w-full">
           <div className="flex flex-col w-full">
             {data?.data.data.map((item, index) => (
-              <div className="w-full px-4 mt-4" key={index}>
-                <Link href={`/Tema/${item.id}`}>
-                    <FotoForo
-                      nombre={<strong>{item.usuario_tema.nombre + ' ' + item.usuario_tema.apellido}</strong>}
-                      description={item.nombre}
-                      idT={item.id}
-                      correo={item.usuario_tema.email}
-                      usuarioCorreo={session?.user?.email}
-                    />
-                </Link>
+              <div className="w-full px-4 mt-4" key={index}>            
+                  <FotoForo
+                    nombre={<strong>{item.usuario_tema.nombre + ' ' + item.usuario_tema.apellido}</strong>}
+                    description={item.nombre}
+                    idT={item.id}
+                    correo={item.usuario_tema.email}
+                    usuarioCorreo={session?.user?.email}
+                    onDeleteSuccess={handleDeleteSuccess}
+                  />             
               </div>
             ))}
           </div>
