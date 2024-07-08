34b08897d8442974ee28741aad432e1f2bb50637
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2024-07-07 20:19:20 -0400

    .

--- src/components/ForoPost.js
+++ src/components/ForoPost.js
@@ -1,34 +1,30 @@
 "use client";
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { obtenerTemas } from '@/queries/foro';
-import { useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react'; // Importa useSession de Next.js
 import FotoForo from './FotoForo'
 import Link from 'next/link';
-
 const ForoPost = ({ className }) => {
-  const queryClient = useQueryClient();
-  const { data, isLoading } = useQuery(['get-foros'], obtenerTemas);
-  const { data: session } = useSession();
+  const { data, isLoading } = useQuery(['get-foros'], () => obtenerTemas());
+  const { data: session } = useSession(); // Obtener los datos de quien lo esta usando ahora
 
-  const handleDeleteSuccess = () => {
-    queryClient.invalidateQueries(['get-foros']);
-  };
-
+  console.log('data', data)
   return (
     <div className={className}>
       <div className="flex flex-col w-full">
         <div className="relative w-full">
           <div className="flex flex-col w-full">
             {data?.data.data.map((item, index) => (
-              <div className="w-full px-4 mt-4" key={index}>            
-                  <FotoForo
-                    nombre={<strong>{item.usuario_tema.nombre + ' ' + item.usuario_tema.apellido}</strong>}
-                    description={item.nombre}
-                    idT={item.id}
-                    correo={item.usuario_tema.email}
-                    usuarioCorreo={session?.user?.email}
-                    onDeleteSuccess={handleDeleteSuccess}
-                  />             
+              <div className="w-full px-4 mt-4" key={index}>
+                <Link href={`/Tema/${item.id}`}>
+                    <FotoForo
+                      nombre={<strong>{item.usuario_tema.nombre + ' ' + item.usuario_tema.apellido}</strong>}
+                      description={item.nombre}
+                      idT={item.id}
+                      correo={item.usuario_tema.email}
+                      usuarioCorreo={session?.user?.email}
+                    />
+                </Link>
               </div>
             ))}
           </div>
--- src/components/FotoForo.js
+++ src/components/FotoForo.js
@@ -4,22 +4,24 @@ import { useMutation } from 'react-query';
 import { Button } from '@nextui-org/button';
 import { TrashIcon } from '@heroicons/react/24/outline';
 
-const Fotoforo = ({ className, nombre, description, idT, correo, usuarioCorreo, onDeleteSuccess }) => {
+const Fotoforo = ({ className, nombre, description, idT, correo, usuarioCorreo }) => {
   const forMutation = useMutation({
     mutationFn: (data) => borrarTema(data),
     onSuccess: () => {
       console.log('Tema eliminado correctamente');
-      onDeleteSuccess(); // Ejecutar callback al eliminar el tema con éxito
+      window.location.reload();
     },
     onError: (error) => {
       console.error('Error al eliminar el tema', error);
     },
   });
-
   const onSubmit = () => {
-    forMutation.mutate({ idT });
+    forMutation.mutate({//envio el id en la varible idT del tema 
+      idT,
+    });
   };
 
+  // Comprueba si el usuario actual es el que iso el tema
   const correoTema = correo === usuarioCorreo;
 
   return (
@@ -29,14 +31,15 @@ const Fotoforo = ({ className, nombre, description, idT, correo, usuarioCorreo, 
         <div className="border-primary-bg justify-center items-center"> {nombre}</div>
       </div>
       <div className="flex-col w-full justify-center items-center text-start text-xl"> {description}</div>
-      {correoTema ? (
+      {correoTema ? ( // muestra el boton si el tema es del usuario que esta usando la pag
         <div className="flex w-1/3 justify-center items-center mr-7">
           <Button type="button" size='md' onClick={onSubmit} className="p-2 rounded bg-red-500 hover:bg-red-700 text-white">
             <TrashIcon className="h-7 w-7" />
           </Button>
         </div>
-      ) : (
-        <div className="flex w-1/3 justify-center items-center mr-7"></div>
+      ): (//si no es el usuario no se muestra nada
+        <div className="flex w-1/3 justify-center items-center mr-7">
+        </div>
       )}
     </div>
   );
