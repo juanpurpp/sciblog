800f2fe27919f8b0e33f3c07238721a5a0783e37
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2024-07-09 11:40:18 -0400

    escpaing de publicaciones

--- src/app/buscar/[q]/page.js
+++ src/app/buscar/[q]/page.js
@@ -11,7 +11,7 @@ import { Spinner } from '@nextui-org/react';
 
 export default function Component({ params }) {
   const [q, setQ] = useState(params.q);
-  const searching = useQuery('search', async () => await Axios.get(`/estudios/buscar?q=${q}`))
+  const searching = useQuery('search', async () => await Axios.get(`/estudios/buscar`, {params:{q:decodeURIComponent(params.q)}}))
   return (
     <div className="bg-white p-8 w-full">
       {/* Primera columna con 3 divs en fila */}
@@ -27,7 +27,7 @@ export default function Component({ params }) {
       </div> : (
         <div className='flex flex-col w-full justify-center space-y-4 items-center'>
         <div className='p-8 w-4/5 space-y-4 flex flex-col'>
-          <h2 className='font-semibold text-3xl'>Resultados de {params.q}</h2>
+          <h2 className='font-semibold text-3xl'>Resultados de {decodeURIComponent(params.q)}</h2>
           {!searching.isLoading && searching.data.data.map((post, index) => (
             <Link key={index} href={`/publicacion/${post.id}`}>
               <PreviewPost titulo={post.titulo} descripcion={post.descripcion} autor={post.usuario_creador.nombre +' '+post.usuario_creador.apellido} tags={post.tags}/>
