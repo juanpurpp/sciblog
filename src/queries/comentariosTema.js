01ec150ea92ba0c4ef9b6ba44a93f846cb8f7f9b
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2024-07-09 01:18:33 -0400

    queries de delete en comentario y respuesta de foros
    -borrarComentario
    -borrarRespuesta
    se debe entregar el id del elemento a borrar.

--- src/queries/comentariosTema.js
+++ src/queries/comentariosTema.js
@@ -1,7 +1,7 @@
 import Axios from "@/services/Axios"
  
 const getComentarioByIdTema = async ({id}) => {
-  
+  //id de tema a comentar
   const response = await Axios.get(`/comentarTema/${id}`)
   return response
 }
@@ -9,4 +9,9 @@ const crearComentario = async ({texto, temaId}) =>{
   const response = await Axios.post('/comentarTema', { texto, temaId } )
   return response
 }
-export {getComentarioByIdTema, crearComentario}
+const borrarComentario = async ({id}) => {
+  //id de comentario a borrar
+  const response = await Axios.delete(`/comentarTema/${id}`)
+  return response
+}
+export {getComentarioByIdTema, crearComentario, borrarComentario}
--- src/queries/respuesta.js
+++ src/queries/respuesta.js
@@ -4,9 +4,14 @@ const crearRespuesta = async ({content, comentarioId}) =>{
   const response = await Axios.post('/respuestaComentario', { content, comentarioId } )
   return response
 }
-
 const getRespuestaByIdComentario = async ({id}) => {
+  //id de comentario a responder
   const response = await Axios.get(`/respuestaComentario/${id}`)
   return response
 }
-export { crearRespuesta , getRespuestaByIdComentario}
+const borrarRespuesta = async ({id}) => {
+  //id de respuesta a borrar
+  const response = await Axios.delete(`/respuestaComentario/${id}`)
+  return response
+}
+export { crearRespuesta , getRespuestaByIdComentario, borrarRespuesta}
